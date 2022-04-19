42232c091cd75fbdea4365e2948d2711c90dbd40
Jérémy <50837004+jrmydix@users.noreply.github.com>
2022-04-19 16:26:39 +0200

    added like animation
    removed heroku procfile
    added like animation

--- assets/app.js
+++ assets/app.js
@@ -27,7 +27,11 @@ settingsCloser.addEventListener("click", () => {
 /**
  * Gestion du thème de couleurs
  */
-if (localStorage.theme === "dark" || (!("theme" in localStorage) && window.matchMedia("(prefers-color-scheme: dark)").matches)) {
+if (
+  localStorage.theme === "dark" ||
+  (!("theme" in localStorage) &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches)
+) {
   document.documentElement.classList.add("dark");
 } else {
   document.documentElement.classList.remove("dark");
@@ -43,4 +47,23 @@ btnTheme.addEventListener("click", function (e) {
     localStorage.theme = "dark";
     document.documentElement.classList.add("dark");
   }
+});
+
+/**
+ * Animation de like
+ */
+const likeBtn = document.querySelector(".heart");
+const likeSvg = document.querySelector(".heart__svg");
+
+likeBtn.addEventListener("click", function () {
+  likeBtn.classList.toggle("liked");
+  likeSvg.classList.toggle("animating");
+});
+
+likeBtn.addEventListener("animationend", function () {
+  likeBtn.classList.toggle("liked");
+  likeSvg.classList.toggle("animating");
+  likeSvg.innerHTML =
+    "<g><path d='M12 21.638h-.014C9.403 21.59 1.95 14.856 1.95 8.478c0-3.064 2.525-5.754 5.403-5.754 2.29 0 3.83 1.58 4.646 2.73.814-1.148 2.354-2.73 4.645-2.73 2.88 0 5.404 2.69 5.404 5.755 0 6.376-7.454 13.11-10.037 13.157H12z'></path></g>";
+  likeSvg.classList.toggle("liked");
 });
